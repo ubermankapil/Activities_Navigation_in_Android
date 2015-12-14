e5ff78f1a5af8b2f947afe7652d67616ea10fdb6
Kapil <kapilintern@gmail.com>
2015-12-14 14:32:47 +0530

    Multiple Activities

---
+++ app/src/main/java/com/app/example/twoactivitiesexample/SecondActivity.java
@@ -0,0 +1,45 @@
+package com.app.example.twoactivitiesexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+    private static String VALUE ="myValue";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void startNewActivity1(View view){
+        Intent intent = new Intent(this, ThirdActivity.class);
+        intent.putExtra(VALUE, "My custom string value");
+        startActivity(intent);
+    }
+}
